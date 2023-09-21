30ba2f9c285936c7104fc16ea16975033cc3dd59
Alex Rodrigues <alex.rodrigues@venturus.org.br>
2023-09-21 19:24:46 -0300

    Updates

--- assets/styles-358f58df.js
+++ assets/styles-358f58df.js
@@ -1,4 +1,4 @@
-import{c as p,d as $,e as S,s,j as l,r as f,f as j,R as x}from"./index-1d238c93.js";function b(e=p){const t=e===p?$:S(e);return function(){const{store:r}=t();return r}}const k=b();function P(e=p){const t=e===p?k:b(e);return function(){return t().dispatch}}const Q=P(),Z=s.button`
+import{c as p,d as $,e as S,s,j as l,r as f,f as j,R as x}from"./index-2a018d47.js";function b(e=p){const t=e===p?$:S(e);return function(){const{store:r}=t();return r}}const k=b();function P(e=p){const t=e===p?k:b(e);return function(){return t().dispatch}}const Q=P(),Z=s.button`
 	padding: 10px 12px;
 	font-size: 14px;
 	font-weight: 600;
